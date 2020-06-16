3afccdd7d56bed2f4c870f1f32bf9fe9a2f993fb
Het Patel <patelhet32@gmail.com>
2020-06-16 01:10:25 -0400

    optmized solver with hashing

--- app/src/main/java/com/example/sudoku/Sudoku.java
+++ app/src/main/java/com/example/sudoku/Sudoku.java
@@ -22,8 +22,21 @@ public class Sudoku{
         box = new boolean[9][9];
         row = new boolean[9][9];
         col = new boolean[9][9];
-        solve(b);
+
+        solver(0, b);
+
         return b;
+    }
+
+    public static int[][] generateRandom() {
+        box = new boolean[9][9];
+        row = new boolean[9][9];
+        col = new boolean[9][9];
+
+        int[][] rand = new int[9][9];
+        solver(0, rand);
+
+        return rand;
     }
 
     public static boolean solver(int p, int[][] b ){
@@ -33,54 +46,26 @@ public class Sudoku{
             if (b[p / 9][p % 9] != 0)
                 continue;
 
-            for (int i = 1; i <= 9; i++) {
-                if (row[y][i-1] || col[x][i-1] || box[3 * ((y) / 3) + ((x) / 3)][i-1])
-                    continue;
-
-                b[p / 9][p % 9] = i;
-                row[y][i-1] = col[x][i-1] = box[3 * ((y) / 3) + ((x) / 3)][i-1] = true;
-
-                if (solver(p + 1, b))
-                    return true;
-
-                b[p / 9][p % 9] = 0;
-                row[y][i-1] = col[x][i-1] = box[3 * ((y) / 3) + ((x) / 3)][i-1] = false;
-            }
-            return false;
-        }
-        return true;
-    }
-
-    public static int[][] generateRandom() {
-        int[][] rand = new int[9][9];
-        box = new boolean[9][9];
-        row = new boolean[9][9];
-        col = new boolean[9][9];
-        return rand;
-    }
-
-    public static boolean randomSolver(int p, int[][] b){
-        for (; p < 81; p++) {
-            int y = p / 9;
-            int x = p % 9;
-            if (b[p / 9][p % 9] != 0)
-                continue;
-
             ArrayList<Integer> li = new ArrayList<Integer>();
             for(int i = 1; i <= 9; i++) li.add(i);
             Collections.shuffle(li);
+
             for (int i : li) {
                 if (row[y][i-1] || col[x][i-1] || box[3 * ((y) / 3) + ((x) / 3)][i-1])
                     continue;
 
                 b[p / 9][p % 9] = i;
-                row[y][i-1] = col[x][i-1] = box[3 * ((y) / 3) + ((x) / 3)][i-1] = true;
+                row[y][i-1] = true;
+                col[x][i-1] = true;
+                box[3 * ((y) / 3) + ((x) / 3)][i-1] = true;
 
                 if (solver(p + 1, b))
                     return true;
 
                 b[p / 9][p % 9] = 0;
-                row[y][i-1] = col[x][i-1] = box[3 * ((y) / 3) + ((x) / 3)][i-1] = false;
+                row[y][i-1] = true;
+                col[x][i-1] = true;
+                box[3 * ((y) / 3) + ((x) / 3)][i-1] = false;
             }
             return false;
         }
